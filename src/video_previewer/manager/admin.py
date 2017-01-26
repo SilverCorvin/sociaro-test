5f988cd94d38ce1aa66bd3caf9a0d8ec9ed92b6a
SilverCorvin <Ivan.Povalyaev@gmail.com>
2017-01-26 13:16:03 +0300

    models were added to admin

--- src/video_previewer/manager/admin.py
+++ src/video_previewer/manager/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from manager.models import Video, VideoCategory
 
 # Register your models here.
+
+
+class VideoAdmin(admin.ModelAdmin):
+    pass
+
+
+class VideoCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Video, VideoAdmin)
+admin.site.register(VideoCategory, VideoCategoryAdmin)
