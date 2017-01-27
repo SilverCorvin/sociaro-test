c539fcd7677878e7b86c27cf9f8a0d19fce4090d
SilverCorvin <Ivan.Povalyaev@gmail.com>
2017-01-27 13:52:32 +0300

    Video Categories data was added

--- run_web.sh
+++ run_web.sh
@@ -5,6 +5,7 @@ sleep 10
 
 cd video_previewer
 
-su -m test -c "python manage.py makemigrations video_previewer"
+su -m test -c "python manage.py makemigrations"
 su -m test -c "python manage.py migrate"
+su -m test -c "python manage.py loaddata --app manager video_category.xml"
 su -m test -c "python manage.py runserver 0.0.0.0:8000"
